58d2115d6ef3db655210955dac7c653dcac99760
lawtonan <lawtonan@mail.gvsu.edu>
2017-10-17 16:45:12 -0400

    Added heading comments to Card and Player

--- Euchre/Euchre/src/game/Card.java
+++ Euchre/Euchre/src/game/Card.java
@@ -1,35 +1,94 @@
 package game;
 
+/**********************************************************************
+ * The Card class is a class that creates Card objects that are used 
+ * in the creation of the deck in Euchre and used to compare in the 
+ * played comparison.
+ * 
+ * @author Andrew Lawton
+ * @author Dan Schroeder
+ * @author Aron Ockerse
+ * @version 1.0
+ *********************************************************************/
 public class Card {
+	
+	/** An integer that keeps track of the card name */
 	private int cardName;
+	
+	/** The suit of the card */
 	private Suits suit;
 	
+	
+	/******************************************************************
+	 * A default constructor that sets card name to -1 and the suit to
+	 * null.
+	 * 
+	 * @param none
+	 *****************************************************************/
 	public Card() {
 		cardName = -1;
 		this.suit = null;
 	}
 	
+	/******************************************************************
+	 * A Constructor that takes an integer for the card name and a suit 
+	 * for the card itself
+	 * 
+	 * @param int card The designated card name
+	 * @param Suits suit the suit the card will be
+	 *****************************************************************/
 	public Card(int card, Suits suit){
 		cardName = card;
 		this.suit = suit;
 	}
 
+	/******************************************************************
+	 * A method that returns card name
+	 * 
+	 * @param none
+	 * @return int returns cardName
+	 *****************************************************************/
 	public int getCardName() {
 		return cardName;
 	}
 
+	/******************************************************************
+	 * A method that sets the card name
+	 * 
+	 * @param int cardName The desired cardName
+	 * @return void
+	 *****************************************************************/
 	public void setCardName(int cardName) {
 		this.cardName = cardName;
 	}
 
+	/******************************************************************
+	 * A method that returns the suit
+	 * 
+	 * @param none
+	 * @return Suits returns suit
+	 *****************************************************************/
 	public Suits getSuit() {
 		return suit;
 	}
 
+	/******************************************************************
+	 * A method that sets the suit
+	 * 
+	 * @param Suits suit The desired suit
+	 * @return void
+	 *****************************************************************/
 	public void setSuit(Suits suit) {
 		this.suit = suit;
 	}
 	
+	/******************************************************************
+	 * A toString method that returns a string for each card depending
+	 * on their set number from cardName.
+	 * 
+	 * @param none
+	 * @return String the Correct string for Card Name
+	 *****************************************************************/
 	public String toString(){
 		if (cardName == 0)
 			return "9 of " + suit;
--- Euchre/Euchre/src/game/Player.java
+++ Euchre/Euchre/src/game/Player.java
@@ -1,44 +1,116 @@
 package game;
 
+/**********************************************************************
+ * The Player class is a class that holds each players information. 
+ * Such as their hand, their team, and whether or not they are the
+ * dealer.
+ * 
+ * @author Andrew Lawton
+ * @author Dan Schroeder
+ * @author Aron Ockerse
+ * @version 1.0
+ *********************************************************************/
 public class Player {
+	
+	/** An array of Cards that designates the hand */
 	public Card hand[] = new Card[5];
+	
+	/** A enumerated type to hold which team the player belongs */
 	public Team team;
+	
+	/** A boolean to determine if the player is the dealer */
 	public boolean dealer;
 	
+	/******************************************************************
+	 * A Constructor that sets the players team and hand, while setting
+	 * dealer to false.
+	 * 
+	 * @param Team team The players team
+	 * @param Card hand[] an array of the players hand
+	 *****************************************************************/
 	public Player(Team team, Card hand[]) {
 		this.team = team;
 		this.hand = hand;
 		dealer = false;
 	}
 
+	/******************************************************************
+	 * A accessor that returns the hand
+	 * 
+	 * @param none
+	 * @return Card[] returns the hand Array
+	 *****************************************************************/
 	public Card[] getHand() {
 		return hand;
 	}
 
+	/******************************************************************
+	 * A mutator that sets the hand.
+	 * 
+	 * @param Card hand[] the players hand to be set
+	 * @return void
+	 *****************************************************************/
 	public void setHand(Card hand[]) {
 		this.hand = hand;
 	}
 	
-	public void setCard(int cnum,Card c) {
+	/******************************************************************
+	 * A mutator to set a card to a certain index in the hand
+	 * 
+	 * @param int cnum the index of the Array
+	 * @param Card c the card to set to the index
+	 *****************************************************************/
+	public void setCard(int cnum, Card c) {
 		this.hand[cnum]=c;
 	}
 	
+	/******************************************************************
+	 * A accessor that returns the Card given by a certain index.
+	 * 
+	 * @param int cnum the index in the array
+	 * @return Card returns the card from the desired index
+	 *****************************************************************/
 	public Card getCard(int cnum) {
 		return this.hand[cnum];
 	}
 
+	/******************************************************************
+	 * A accessor that returns the players team
+	 * 
+	 * @param none
+	 * @return Team returns team
+	 *****************************************************************/
 	public Team getTeam() {
 		return team;
 	}
 
+	/******************************************************************
+	 * A mutator that sets the team of the player
+	 * 
+	 * @param Team team The desired team to set
+	 * @return void
+	 *****************************************************************/
 	public void setTeam(Team team) {
 		this.team = team;
 	}
 
+	/******************************************************************
+	 * A method that returns if the player is the dealer or not
+	 * 
+	 * @param none
+	 * @return boolean returns dealer
+	 *****************************************************************/
 	public boolean isDealer() {
 		return dealer;
 	}
 
+	/******************************************************************
+	 * A method that sets the boolean to whether or not the player is
+	 * the dealer.
+	 * 
+	 * @param boolean dealer the desired boolean for this player
+	 * @return void
+	 *****************************************************************/
 	public void setDealer(boolean dealer) {
 		this.dealer = dealer;
 	}
