5e0e8ac70f13a3e2585fe2e113d7e5f1dd96c1e7
lawtonan <lawtonan@mail.gvsu.edu>
2017-10-18 10:44:46 -0400

    Changed Working Directory and Docs

---
+++ Euchre/src/game/EuchreTest.java
@@ -0,0 +1,60 @@
+package game;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+
+import org.junit.Test;
+
+public class EuchreTest {
+
+	@Test
+	public void testCreateDeck() {
+		Euchre junit = new Euchre();
+		ArrayList<Card> result = junit.createDeck();
+		ArrayList<Card> test = new ArrayList<Card>();
+		for (int i = 0; i <= 5; i++) {
+			test.add(new Card(i, Suits.Diamonds));
+			test.add(new Card(i, Suits.Hearts));
+			test.add(new Card(i, Suits.Clubs));
+			test.add(new Card(i, Suits.Spades));
+		}
+		test = junit.createDeck();
+
+		assertArrayEquals(result.toArray(), test.toArray());
+
+	}
+
+	@Test
+	public void testPrintCards() {
+		Euchre junit = new Euchre();
+		ArrayList<Card> deck = junit.createDeck();
+
+		assertNotNull(deck);
+
+	}
+
+	@Test
+ 	public void testGameStatus() {
+		Euchre junit = new Euchre();
+		junit.T1Point();
+
+		assertFalse(junit.gameStatus());
+	}
+
+	@Test
+	public void testShuffle() {
+		Euchre junit = new Euchre();
+
+		ArrayList<Card> deck = junit.createDeck();
+		ArrayList<Card> deck2 = junit.createDeck();
+
+		junit.shuffleTest(deck);
+
+		assertNotSame(deck.toArray(), deck2.toArray());
+	}
+
+}
---
+++ Euchre/src/game/Team.java
@@ -0,0 +1,5 @@
+package game;
+
+public enum Team {
+	Team1, Team2
+}
